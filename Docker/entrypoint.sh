f0f93349b8314263ea7098c36d313ccd00ad19db
Sam2600 <kghtetsan26@gmail.com>
2024-03-28 23:13:05 +0630

    added docker files

---
+++ Docker/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -f vendor/autoload.php ]; then
+    composer install
+fi
+
+if [ ! -f ".env" ]; then
+    echo "Creating an env file for $APP_ENV"
+    cp env.example .env
+else
+    echo "Env file is found."
+fi
+
+
+php artisan optimize:clear
+php artisan key:generate
+
+php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
+
+exec docker-php-entrypoint "$@"
---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM php:apache as php
+
+WORKDIR /var/www/html
+
+RUN apt-get update -y &&\
+    apt-get install -y unzip git nodejs
+
+RUN docker-php-ext-install pdo pdo_mysql mysqli
+
+COPY --from=composer:2.3.5 /usr/bin/composer /usr/bin/composer
+
+COPY . .
+
+ENV PORT=8000
+ENTRYPOINT [ "Docker/entrypoint.sh" ]
